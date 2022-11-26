3cdcb6cf4a2982ec52d0dd25aeee799c18e0cecb
Robert <g00395612@atu.ie>
2022-11-26 15:28:11 +0000

    First Commit

---
+++ src/main/java/ie/atu/MovieProjectCICD/MovieProjectCicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.MovieProjectCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieProjectCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieProjectCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/MovieProjectCICD/MovieProjectCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.MovieProjectCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProjectCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
