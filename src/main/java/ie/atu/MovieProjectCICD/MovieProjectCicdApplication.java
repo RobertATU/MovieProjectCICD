69acc883cb2acb14910fa28d69b7fdec73da0516
Robert <g00395612@atu.ie>
2022-12-05 20:04:22 +0000

    Quick comment out for test purpose

--- src/main/java/ie/atu/MovieProjectCICD/MovieProjectCicdApplication.java
+++ src/main/java/ie/atu/MovieProjectCICD/MovieProjectCicdApplication.java
@@ -3,11 +3,12 @@ package ie.atu.MovieProjectCICD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MovieProjectCicdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieProjectCicdApplication.class, args);
-	}
+	//public static void main(String[] args) {
+	//
+		//SpringApplication.run(MovieProjectCicdApplication.class, args);
+	//}
 
 }
